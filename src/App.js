b2a29583ad85bfd48e1b599a0df10e6988890528
aliali2003 <aliali2003@MacBook-Air-aliali2003.local>
2021-10-29 01:43:32 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -2,35 +2,41 @@ import React from 'react';
 import './App.css';
 
 
-class Square extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         value: null
-    //     };
-    // }
+// class Square extends React.Component {
+//
+//     render() {
+//         return (
+//             <button className="square" onClick={ ()=> this.props.onClick()}>
+//                 {this.props.value}
+//             </button>
+//         );
+//     }
+// }
 
-    render() {
-        return (
-            <button className="square" onClick={ ()=> this.props.onClick()}>
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
 
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true
         }
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
 
@@ -44,7 +50,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
